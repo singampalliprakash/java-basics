f3e6e8a26289fa3638199fcff6b3a31de46566ff
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-01-29 23:35:35 +0530

    Add files via upload

---
+++ java basics1.java
@@ -0,0 +1,68 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int m1=50,m2=60,m3=70,m4=80;
+        int avg=(m1+m2+m3+m4)/4;
+        if(avg>=40){
+            if (avg>=70){
+                System.out.println("destination");
+            }
+            else if (avg>=60){
+                System.out.println("A Grade");
+                
+            }
+            else if (avg>=50){
+                System.out.println("B Grade");
+            }
+            else if (avg>=40){
+                System.out.println("C Grade");
+            }
+        }
+        else
+        {
+            System.out.println("fail");
+        }
+        
+        
+        
+        
+        
+        // int a=5,b=6,c=78;
+        // if (a>b && a>c){
+        //     System.out.println("a is bigger");
+        // }
+        // else if (b>c){
+        //     System.out.println("b is bigger");
+        // }
+        // else{
+        //     System.out.println("c is bigger");
+        // }
+        
+        
+        
+        // int x=10000;
+        // int f=24*60*60;
+        // int days=(x/f);
+        // int y= (x%f);
+        // int hours=y/(60*60);
+        // int z=y%(60*60);
+        // int minutes=(z/60);
+        // int seconds=z%60;
+        // System.out.println("days:"+days+","+"hours:"+hours+","+"minutes:"+minutes+","+"seconds:"+seconds);
+        
+        
+// 	    int age=25;
+// 	    if (age>=18 && age<=30)
+// 	    System.out.println("you are young you can participate");
+// 	    else
+// 	    System.out.println("better luck next time");
+	    
+	    
+	    
+// 	    int principalAmount=80000;
+// 	    int time=3;
+// 	    double rateofInterest=3.5;
+// 	    double sI=(principalAmount*time*rateofInterest)/100;
+// 	    double totalAmount=sI+principalAmount;
+// 		System.out.println("simple interest:"+sI);
+// 		System.out.println("totalAmount:"+totalAmount);
